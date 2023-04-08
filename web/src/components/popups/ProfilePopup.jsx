8897cf54d79c95423b09f53c6f100568991fe925
Olha Horban <horbanolha64@gmail.com>
2023-04-08 21:58:02 +0300

    customer request

--- web/src/components/popups/ProfilePopup.jsx
+++ web/src/components/popups/ProfilePopup.jsx
@@ -1,10 +1,10 @@
-import React, {useContext, useState} from 'react';
+import React from 'react';
 import RoundButton from "../UI/buttons/RoundButton";
 import phone from '../../assets/images/phone.png'
 import location from '../../assets/images/address.png'
 
 const ProfilePopup = ({setVisible}) => {
-    const employee = JSON.parse(localStorage.getItem('employee'));
+    const employee = localStorage.getItem('employee');
     const st = {
         color: 'white',
         fontSize: '16px'
@@ -56,14 +56,14 @@ const ProfilePopup = ({setVisible}) => {
                 <img src={phone} height="25px" width="25px" className="profile-v"/>
                 <div className="profile-nl-content" >
                     <div style={st}>Телефон</div>
-                    <div style={st1}>{employee.phone}</div>
+                    <div style={st1}>+380684951236</div>
                 </div>
             </div>
             <div className="profile-nl">
                 <img src={location} width="18px" height="25px" className="profile-v"/>
                 <div className="profile-nl-content">
                     <div style={st}>Адреса</div>
-                    <div style={st1}>{employee.city}, {employee.street}, {employee.zip}</div>
+                    <div style={st1}>Київ, вул. Марини Цвєтаєвої, 24300</div>
                 </div>
             </div>
         </div>
